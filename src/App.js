1a55daec2ad13c1fda26a5ec12c1e5c9dd1e3683
Leonardo Souza <leonardops91@gmail.com>
2021-05-18 10:45:04 -0300

    Front quase pronto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './assets/Logo.png';
+import foto from './assets/Photo.png';
+import icon_code from './assets/icon_code.svg';
+import icon_users from './assets/icon_users.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="logo" alt="Logo AluraDev" />
+        <input type="text" name="search" className="input-text" id="search" placeholder="Busque por algo" />
+        <div className="info-user">
+          <img src={foto} alt="Foto user" />
+          <span className="user-name">Harry</span>
+        </div>
       </header>
-    </div>
+
+      <body className="app-body">
+        <div className="app-menu">
+          <p className="menu-title">Menu</p>
+          <ul>
+            <li>
+              <a href="http://localhost:3000"><div className="menu-icon" id="editor-icon"><img src={icon_code} alt="icon" /></div>Editor de código</a>
+            </li>
+            <li>
+              <a href="http://localhost:3000"><div className="menu-icon" id="comunity-icon"><img src={icon_users} alt="icon" /></div>Comunidade</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="app-content"></div>
+
+        <div className="app-form">
+          <p className="menu-title">seu projeto</p>
+          <input type="text" name="project-name" id="project-name" className="input-text" placeholder="Nome do seu projeto" />
+          <input type="text" name="project-description" id="project-description" className="input-text" placeholder="Descrição do seu projeto" />
+          <p className="menu-title">personalização</p>
+          <select name="input-language" className="input-text" id="input-language">
+            <option value="Javascipt">Javascript</option>
+            <option value="HTML">HTML</option>
+            <option value="CSS">CSS</option>
+          </select>
+          <input type="color" icon_code="blue" name="color-pick" id="color-pick" />
+          <input type="button" value="Salvar projeto" className="button" />
+        </div>
+      </body>
+    </div >
   );
 }
 
